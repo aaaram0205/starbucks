ab74de7e83d4596dade40ba05c7616a3f810f0bb
aaaram0205 <aaaram0205@gmail.com>
2022-05-16 23:59:21 +0900

    lodash: _.throttle/ gsap

--- js/main.js
+++ js/main.js
@@ -6,11 +6,13 @@ searchEl.addEventListener ('click', function (){
     searchInputEl.focus ();
 });
 
+
 //input focus되면
 searchInputEl.addEventListener ('focus', function(){
     searchEl.classList.add('focused');
     searchInputEl.setAttribute('placeholder','통합검색');
 });
+
 
 //input focus 해제되면
 searchInputEl.addEventListener ('blur', function(){
@@ -18,3 +20,20 @@ searchInputEl.addEventListener ('blur', function(){
     searchInputEl.setAttribute('placeholder','');
 });
 
+
+// 함수가 스크롤 될때마다 함수가 실행되는게 성능을 떨어뜨림. 스크롤 될 때마다 실행되는 함수를 외부에서 가져오는 'js라이브러리'로 제어 (lodash.js cdn)
+//_.throttle (함수, 시간): 스크롤될때 실행되는 함수의 개수를 일정시간동안에 한번씩만 실행되도록 제한하는 메서드.
+
+//js 애니메이션 라이브러리 gsap: gsap.to (처리할 요소, 지속시간, 옵션);
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener ('scroll', _.throttle(function(){
+    if (window.scrollY > 500) {
+        //배지 숨기기
+        //badgeEl.style.display = "none";
+    } else {
+        //배지 보이기
+       // badgeEl.style.display = "block";
+    }
+},300));
