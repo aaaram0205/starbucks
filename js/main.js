6d707733618ccd0978545da730196b446c683b0f
aaaram0205 <aaaram0205@gmail.com>
2022-05-23 15:16:41 +0900

    gsap을 이용한 floating animation/ fixed background

--- js/main.js
+++ js/main.js
@@ -101,3 +101,29 @@ promotionToggleBtn.addEventListener('click', function(){
     }
 });
 
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+    // `.toFixed()`를 통해 반환된 문자 데이터를,
+    // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+    return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floatingObject (selector, delay, size) {
+    //gsap.to(요소, 지속시간, 객체옵션)
+    gsap.to (
+        selector,   // 1. 선택자
+        random(1.5, 2.5),    //2. 애니메이션 동작시간 (랜덤함수로 랜덤)
+        {   // 3. 옵션
+            y: size,
+            repeat: -1,  // 무한반복
+            yoyo: true,  // 수행한 동작 역으로 다시 동작
+            delay: random(0, delay),   // 랜덤하게 바로시작(0)해서 내가 받아온 delay 매개변수 값 사이에서 지연시킴.
+            ease: Power1.easeInOut
+        }
+    );
+}
+
+floatingObject ('.floating1', 1, 15);   //.floating1가 delay:1초, 15px 위아래로 움직이게
+floatingObject ('.floating2', .5, 15);
+floatingObject ('.floating3', 1.5, 20);
